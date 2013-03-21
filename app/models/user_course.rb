e5d767031cd673a11a238959f49e8f049188c8bc
Tim Hunter <hunternz@gmail.com>
2013-03-21 16:41:55 -0400

    Commented

--- app/models/user_course.rb
+++ app/models/user_course.rb
@@ -7,10 +7,15 @@ class UserCourse < ActiveRecord::Base
   # scope :codeschool, where :course_provider_id => 1
 
   def self.codeschool
+    #creates a result set that is all CodeSchool user courses. 
+    #This is a custom sql query. Also, the readonly(false) attribute 
+    #overrides the default behavior of join results being read only.
+    #Why? We need to figure this out.    
     joins(:course).where("courses.course_provider_id = ?", 1).readonly(false)
   end
 
   def self.codeschool_for_user(user)
+    # this builds on self.codeschool to further limit it to specific user.
     self.codeschool.where(:user_id => user.id)
   end
 
