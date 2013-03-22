b65aa554f30585b3c91e40e64552ffa9f6913b51
Masha Rikhter <masha.rikhter@gmail.com>
2013-03-22 14:35:13 -0400

    overall progress %

--- app/controllers/prework_controller.rb
+++ app/controllers/prework_controller.rb
@@ -7,6 +7,11 @@ class PreworkController < ApplicationController
       user_id = params[:id]
     end
     @user_courses = User.find(user_id).user_courses
+
+    student = User.first
+    all_progress = student.user_courses.select(:progress)
+    progress_array = all_progress.map {|a| a.progress}
+    @average = progress_array.inject{ |sum, el| sum + el }.to_f / progress_array.size
 	end
 
 end
