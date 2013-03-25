c45acf15e63691e898a66ea6de4083ebfe219ca2
Masha Rikhter <masha.rikhter@gmail.com>
2013-03-25 10:10:01 -0400

    sort by overall %

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,7 +3,7 @@ ActiveAdmin.register_page "Dashboard" do
   menu :priority => 1, :label => proc{ I18n.t("active_admin.dashboard") }
 
   content do
-  
+
     panel "Student Progress" do
       table_for User.all.sort_by {|u| u.overall_progress} do
         column :first_name, :sortable => "first_name"
