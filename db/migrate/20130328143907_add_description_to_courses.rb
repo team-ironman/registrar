3aede4fd5f296610b8113546e70da75717f4c3dd
Erin L <email.erin@gmail.com>
2013-03-28 16:20:03 -0400

    new columns to subject and course for description

---
+++ db/migrate/20130328143907_add_description_to_courses.rb
@@ -0,0 +1,4 @@
+class AddDescriptionToCourses < ActiveRecord::Migration
+  def change
+  end
+end
---
+++ db/migrate/20130328144450_add_descip_to_courses.rb
@@ -0,0 +1,5 @@
+class AddDescipToCourses < ActiveRecord::Migration
+  def change
+    add_column :courses, :description, :string
+  end
+end
