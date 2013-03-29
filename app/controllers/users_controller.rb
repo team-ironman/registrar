d1d63ed44797fd7e476a9b874067eaa68db5aefd
firstgeneration <daniel.gantz@gmail.com>
2013-03-29 16:19:42 -0400

    update email code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,6 +35,7 @@ class UsersController < ApplicationController
         session[:user_id] = @user.id
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render json: @user, status: :created, location: @user }
+
       else
         format.html { render action: "new" }
         format.json { render json: @user.errors, status: :unprocessable_entity }
