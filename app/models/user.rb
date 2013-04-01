8e984446a6f29b474194c1706dd16170b58fea36
Masha Rikhter <masha.rikhter@gmail.com>
2013-04-01 10:42:47 -0400

    dashboard and user full name update

--- app/models/user.rb
+++ app/models/user.rb
@@ -59,6 +59,15 @@ class User < ActiveRecord::Base
     self.user_courses.build(:course_id => course.id).save
   end
 
+  def full_name
+    [first_name, last_name].join(' ')
+  end
+
+  # def full_name=(name)
+  #   split = name.split(' ',2)
+  #   self.first_name = split.first
+  #   self.last_name = split.last
+  # end
 
   private 
   
