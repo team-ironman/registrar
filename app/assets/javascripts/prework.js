fe5d3d7c158f83228867bd31109799d0e66692d0
Erin L <email.erin@gmail.com>
2013-04-02 10:48:17 -0400

    change text to icons, placeholder for timer

--- app/assets/javascripts/prework.js
+++ app/assets/javascripts/prework.js
@@ -1,3 +1,5 @@
+
+
 /*!
  * jQuery timer plugin: display a timer inside a div or span or any valid html tag
  * Examples and documentation at: http://walmik.info/demos/timer.jquery
@@ -16,116 +18,116 @@
  
   
 */
-;(function($)
-{
-    var secs = 0;
-    var mins = 0;
-    var hrs = 0;
-    var secsStr = "00";
-    var minsStr = "00";
-    var hrsStr = "00";
-    var timerId = null;
-    var delay = 1000;
-    var type = "string";
-    var isTimerRunning = false;
+// ;(function($)
+// {
+//     var secs = 0;
+//     var mins = 0;
+//     var hrs = 0;
+//     var secsStr = "00";
+//     var minsStr = "00";
+//     var hrsStr = "00";
+//     var timerId = null;
+//     var delay = 1000;
+//     var type = "string";
+//     var isTimerRunning = false;
     
     
-    $.fn.timer = function(method, options)
-    {
-        var element = this;
-        var settings = $.extend({showHours: false}, options);
+//     $.fn.timer = function(method, options)
+//     {
+//         var element = this;
+//         var settings = $.extend({showHours: false}, options);
         
-        if (settings.seconds !== undefined)
-        {
-            hrs = Math.floor(settings.seconds / 3600);
-            mins = Math.floor((settings.seconds - (hrs * 3600))/60);
-            secs = settings.seconds - (hrs * 3600) - (mins * 60);
+//         if (settings.seconds !== undefined)
+//         {
+//             hrs = Math.floor(settings.seconds / 3600);
+//             mins = Math.floor((settings.seconds - (hrs * 3600))/60);
+//             secs = settings.seconds - (hrs * 3600) - (mins * 60);
             
-            timeToString();
-        }
+//             timeToString();
+//         }
         
-        switch(method)
-        {
-            case "start":
-                if(!isTimerRunning) startTimer();
-                break;
+//         switch(method)
+//         {
+//             case "start":
+//                 if(!isTimerRunning) startTimer();
+//                 break;
             
-            case "pause":
-                pauseTimer();
-                break;
+//             case "pause":
+//                 pauseTimer();
+//                 break;
             
-            case "resume":
-                if(!isTimerRunning) startTimerInterval();
-                break;
+//             case "resume":
+//                 if(!isTimerRunning) startTimerInterval();
+//                 break;
             
-            case "reset":
-                secs = 0;
-                mins = 0;
-                hrs = 0;
-                break;
+//             case "reset":
+//                 secs = 0;
+//                 mins = 0;
+//                 hrs = 0;
+//                 break;
             
-            case "get_seconds":
-                return ( (hrs*3600) + (mins*60) + secs - 1 );
-                break;
-        }
+//             case "get_seconds":
+//                 return ( (hrs*3600) + (mins*60) + secs - 1 );
+//                 break;
+//         }
 
-        function pauseTimer()
-        {
-            clearInterval(timerId);
-            isTimerRunning = false;
-        }
+//         function pauseTimer()
+//         {
+//             clearInterval(timerId);
+//             isTimerRunning = false;
+//         }
         
-        function startTimer()
-        {
-            updateTimerDisplay();
-            incrementTime(); //to avoid the 1 second gap that gets created if the seconds are not incremented
-            startTimerInterval();
-        }
+//         function startTimer()
+//         {
+//             updateTimerDisplay();
+//             incrementTime(); //to avoid the 1 second gap that gets created if the seconds are not incremented
+//             startTimerInterval();
+//         }
         
-        function startTimerInterval()
-        {
-            timerId = setInterval(incrementTime, delay);
-            isTimerRunning = true;
-        }
+//         function startTimerInterval()
+//         {
+//             timerId = setInterval(incrementTime, delay);
+//             isTimerRunning = true;
+//         }
         
-        function updateTimerDisplay()
-        {
-            if(hrs > 0) settings.showHours = true;
-            if(settings.showHours) $(element).html(hrsStr + ":" + minsStr + ":" + secsStr);
-            else $(element).html(minsStr + ":" + secsStr);
-        }
+//         function updateTimerDisplay()
+//         {
+//             if(hrs > 0) settings.showHours = true;
+//             if(settings.showHours) $(element).html(hrsStr + ":" + minsStr + ":" + secsStr);
+//             else $(element).html(minsStr + ":" + secsStr);
+//         }
         
-        function timeToString()
-        {
-            if(secs < 10) secsStr = "0" + secs;
-            else secsStr = secs;
+//         function timeToString()
+//         {
+//             if(secs < 10) secsStr = "0" + secs;
+//             else secsStr = secs;
             
-            if(mins < 10) minsStr = "0" + mins;
-            else minsStr = mins;
+//             if(mins < 10) minsStr = "0" + mins;
+//             else minsStr = mins;
             
-            if(hrs < 10) hrsStr = "0" + hrs;
-            else hrsStr = hrs;
-        }
+//             if(hrs < 10) hrsStr = "0" + hrs;
+//             else hrsStr = hrs;
+//         }
         
-        function incrementTime()
-        {
-            timeToString();
-            updateTimerDisplay();
+//         function incrementTime()
+//         {
+//             timeToString();
+//             updateTimerDisplay();
             
-            secs++;
-            if(secs % 60 == 0)
-            {
-                mins++;
-                secs = 0;
-            }
+//             secs++;
+//             if(secs % 60 == 0)
+//             {
+//                 mins++;
+//                 secs = 0;
+//             }
             
-            //handle time exceeding 60 mins!
-            if(mins > 59 && mins % 60 == 0)
-            {
-                hrs++;
-                mins = 0;
-            }
-        }
+//             //handle time exceeding 60 mins!
+//             if(mins > 59 && mins % 60 == 0)
+//             {
+//                 hrs++;
+//                 mins = 0;
+//             }
+//         }
         
-    }
-})(jQuery);
+//     }
+// })(jQuery);
