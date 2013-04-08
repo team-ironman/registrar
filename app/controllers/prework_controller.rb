331e86958ce87728327b6e46ca5f91d13dc8688c
Tim Hunter <hunternz@gmail.com>
2013-04-07 23:59:19 -0400

    Prework page will not error if user is not logged you. They will now get directed to session/new. It should work fine, but needs more testing.

--- app/controllers/prework_controller.rb
+++ app/controllers/prework_controller.rb
@@ -1,21 +1,21 @@
 class PreworkController < ApplicationController
 
   def index
-    if !params[:id]
-     user_id = session[:user_id]
-    else
-      user_id = params[:id]
+    
+    redirect_to new_session_path if session[:user_id].blank?
+    
+    if user_id = session[:user_id]
+      @user_courses = Course.courses_for_user(user_id)
+
+      @student = User.find(user_id)
+
+      # all_progress = @student.user_courses.select(:progress)
+      # progress_array = all_progress.map {|a| a.progress}
+      # @average = progress_array.inject{ |sum, el| sum + el }.to_f / progress_array.size
+      @average = @student.overall_progress.round(0)
+  
     end
 
-    @user_courses = Course.courses_for_user(user_id)
-
-
-    @student = User.find(user_id)
-
-    # all_progress = @student.user_courses.select(:progress)
-    # progress_array = all_progress.map {|a| a.progress}
-    # @average = progress_array.inject{ |sum, el| sum + el }.to_f / progress_array.size
-    @average = @student.overall_progress.round(0)
-    end
+  end
 
 end
--- config/schedule.rb
+++ config/schedule.rb
@@ -22,6 +22,6 @@
 set :environment, "development"
 set :output, "log/cron.log"
 
-every 2.minutes do
+every 12.hours do
   rake "update_progress"
 end
