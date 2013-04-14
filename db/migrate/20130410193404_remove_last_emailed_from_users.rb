1470f272f58aa151ddbf05207fe635d412066595
Tim Hunter <hunternz@gmail.com>
2013-04-14 19:23:17 -0400

    changed the migrations so we only have a time type rather than date_time so postgres doesn't blow up.

--- db/migrate/20130410193404_remove_last_emailed_from_users.rb
+++ db/migrate/20130410193404_remove_last_emailed_from_users.rb
@@ -1,11 +1,11 @@
 class RemoveLastEmailedFromUsers < ActiveRecord::Migration
   def up
     remove_column :users, :last_emailed
-    add_column :users, :last_emailed, :date_time
+    add_column :users, :last_emailed, :time
   end
 
   def down
-    remove_column :users, :last_emailed, :date_time
+    remove_column :users, :last_emailed
     add_column :users, :last_emailed, :time
   end
 end
