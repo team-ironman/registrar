262c38315288f7226ae8c5178d9f709521a7e7a4
firstgeneration <daniel.gantz@gmail.com>
2013-04-17 11:36:51 -0400

    debugger removed from Progress controller

--- app/controllers/progress_controller.rb
+++ app/controllers/progress_controller.rb
@@ -1,7 +1,6 @@
 class ProgressController < ApplicationController
 
   def index
-    debugger
     @semester = Semester.first
     @days_before_semester = (@semester.start.to_date - Time.now.to_date).to_i
   end
