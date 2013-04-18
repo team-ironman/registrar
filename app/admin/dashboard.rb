ce6246e65a280153725b81a29db7b6b2d64c238c
Tim Hunter <hunternz@gmail.com>
2013-04-18 00:54:44 -0400

    time

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -6,7 +6,11 @@
 
   # menu :priority => 1, :label => proc{ I18n.t("active_admin.dashboard") }
 
+
+
 ActiveAdmin::Dashboards.build do
+
+ # include ActionView::Helpers::DateHelper
 
     section "Student Progress" do
       
@@ -15,7 +19,7 @@ ActiveAdmin::Dashboards.build do
         t.column("Progress")        { |n| number_to_percentage(n.overall_progress, precision: 0) }
         t.column("Past Due")        { |n| "#{n.past_due_courses(21).size} Past Due" }
         t.column("Select Students") { |n| check_box_tag('user_ids[]', n.id, false, :class => 'user_ids') }
-        t.column("Last emailed")    { |n| n.last_emailed.time_ago_in_words }
+        t.column("Last emailed")    { |n| n.last_emailed }
         t.column(button_to("New Email", new_admin_email_path, :method => :get, :id => 'test')) {}
 
       end
