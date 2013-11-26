6fa3e4a8f266409ff8e6cd7fc1aa4658e9f144d4
Matt Campbell <mecampbell25@gmail.com>
2013-11-26 13:42:38 -0500

    config files

---
+++ TODO.md
@@ -0,0 +1,9 @@
+#TODOs for Registrar App
+
+##Versioning
+1. Update to Rails 4
+1. 
+
+##Aesthetic
+
+##Other
--- db/schema.rb
+++ db/schema.rb
@@ -57,12 +57,12 @@ ActiveRecord::Schema.define(:version => 20130418041048) do
     t.string   "url"
     t.decimal  "estimated_time"
     t.integer  "subject_id"
-    t.datetime "created_at",                           :null => false
-    t.datetime "updated_at",                           :null => false
+    t.datetime "created_at",            :null => false
+    t.datetime "updated_at",            :null => false
     t.integer  "treehouse_badges"
     t.integer  "course_provider_id"
     t.integer  "days_due_before_class"
-    t.text     "description",           :limit => 255
+    t.text     "description"
   end
 
   create_table "email_users", :force => true do |t|
@@ -111,10 +111,10 @@ ActiveRecord::Schema.define(:version => 20130418041048) do
 
   create_table "subjects", :force => true do |t|
     t.string   "name"
-    t.datetime "created_at",                                  :null => false
-    t.datetime "updated_at",                                  :null => false
-    t.integer  "display_order",                :default => 0
-    t.text     "description",   :limit => 255
+    t.datetime "created_at",                   :null => false
+    t.datetime "updated_at",                   :null => false
+    t.integer  "display_order", :default => 0
+    t.text     "description"
   end
 
   create_table "user_courses", :force => true do |t|
